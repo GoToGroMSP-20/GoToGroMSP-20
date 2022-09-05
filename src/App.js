bb1c6c3da429aad8389d5cb2b7319ba0226ffa84
khang-movember <88306335+khang-movember@users.noreply.github.com>
2022-09-05 17:34:48 +1000

    feat: added reusable button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from './components/Button/Button';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  function alertBrowser() {
+    alert("this be alert")
+  }
+  function consoleLogBrowser() {
+    console.log("this be log")
+  }
+  return(
+    <div>
+      <Button buttonName={"This is a button."} onClickFunction={alertBrowser}/>
+      <Button buttonName={"different button."} onClickFunction={consoleLogBrowser}/>
     </div>
   );
-}
+};
 
 export default App;
