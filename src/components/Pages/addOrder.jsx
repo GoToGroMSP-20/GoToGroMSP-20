075a8cff1067ecd929f2defafe257ee5af38c496
Baavi <87794520+Baavi@users.noreply.github.com>
2022-09-23 14:53:24 +1000

    more changes

--- src/components/Pages/addOrder.jsx
+++ src/components/Pages/addOrder.jsx
@@ -1,23 +1,20 @@
 import React from "react";
 import { Button } from "../Button/Button";
 import { InputField } from "../InputField/InputField";
-//import "./AddOrder.css";
+import "./AddOrder.css";
 
 export const AddOrder = () => {
   return (
     <div className="AddOrder">
-      <h1 className="App-header">Add New Order</h1>
+      <h1 className="white-text">Add New Order</h1>
       <form>
         <InputField
           label={"Order ID"}
-          placeholder={"A78D4S5S5"}
-          id={"transaction_id"}
+          name={"transaction_id"}
         />
         <InputField
           label={"Member ID"}
-          id={"member_id"}
-          type={"email"}
-          placeholder={"alfie.santos@gmail.com"}
+          name={"member_id"}
         />
         <Button buttonType={"primary"} isDisabled={true}>
           Add Order
