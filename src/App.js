82ce81c977b5928b4057b7d31b98acc867c5e04e
ShiroKatake <tgkhang262@gmail.com>
2022-09-26 12:56:35 +1000

    feat(router): added router for forms

--- src/App.js
+++ src/App.js
@@ -1,54 +1,27 @@
-
-import logo from "./logo.svg";
-import { useState } from "react";
 import './App.css';
-import { Button } from './components/Button/Button';
-import { InputField } from './components/InputField/InputField';
 import { AddMember } from './pages/AddMember/AddMember';
-import { AddOrder } from "./components/Pages/addOrder";
-
-
-function alertBrowser() {
-  alert("Function executed");
-  console.log("Function executed");
-}
+import { AddOrder } from "./pages/AddOrder/AddOrder";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [values, setValues] = useState({
-    firstName: "",
-    dob: "",
-  });
+  // const [values, setValues] = useState({
+  //   firstName: "",
+  //   dob: "",
+  // });
 
-  const inputs = [
-    {
-      id: 1,
-      label: "First Name",
-      name: "firstName",
-      type: "text",
-      placeHolder: "firstname",
-      required: true,
-      errorMessage: "This didn't work rip",
-      successMessage: "This worked yay!",
-    },
-    {
-      id: 2,
-      label: "Date Of Birth",
-      name: "dob",
-      type: "date",
-      required: true,
-      errorMessage: "This didn't work rip",
-      successMessage: "This worked yay!",
-    },
-  ];
-
-  const onChange = (e) => {
-    setValues({ ...values, [e.target.name]: e.target.value });
-  };
+  // const onChange = (e) => {
+  //   setValues({ ...values, [e.target.name]: e.target.value });
+  // };
 
   return (
     <div className="App">
-      <AddOrder />
-      <AddMember/>
+      <Router basename='/GoToGroMSP-20'>
+        <Routes>
+          <Route path="/" element={<AddOrder/>} />
+          <Route path="/add-order" element={<AddOrder/>} />
+          <Route path="/add-member" element={<AddMember/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
